5e24e298bf78d6d853544d23ed0334fbd2191f32
Clint Nathan Calumpad <72256679+kwasonlover@users.noreply.github.com>
2023-11-21 14:48:50 +0800

    chore: added comments to code

--- app/api/courses/createCourse/route.js
+++ app/api/courses/createCourse/route.js
@@ -3,9 +3,14 @@ import Course from "@models/courseModel";
 export const POST = async (req) => {
   const { userId, course, students, days } = await req.json();
   try {
+    // connect to mongoDB
     await connectToDB();
+
+    // pass variables above, then save in the db
     const newCourse = new Course({ professor: userId, course, students, days });
     await newCourse.save();
+
+    // return response
     return new Response(JSON.stringify(newCourse), { status: 201 });
   } catch (error) {
     return new Response("Failed to create a new Course.", { status: 500 });
