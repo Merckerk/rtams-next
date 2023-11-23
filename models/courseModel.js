4f3d9478168972a46ddc1cb07b1c39728b1574e1
Clint Nathan Calumpad <72256679+kwasonlover@users.noreply.github.com>
2023-11-24 01:54:05 +0800

    chore: added term field in course schema

--- models/courseModel.js
+++ models/courseModel.js
@@ -1,5 +1,13 @@
 import { Schema, model, models } from "mongoose";
 
+const Term = {
+  "2023-2024": "2023-2024",
+  "2024-2025": "2024-2025",
+  "2025-2026": "2025-2026",
+  "2026-2027": "2026-2027",
+  "2027-2028": "2027-2028",
+};
+
 const courseSchema = new Schema({
   courseName: {
     type: String,
@@ -10,6 +18,10 @@ const courseSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+  },
+  term: {
+    type: String,
+    enum: Object.values(Term),
   },
 });
 
