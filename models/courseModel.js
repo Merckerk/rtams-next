f133bf0ac8784a8a287b75b1114a689674b7aa77
Clint Nathan Calumpad <72256679+kwasonlover@users.noreply.github.com>
2023-11-24 00:03:53 +0800

    chore: removed unnecessary fields in courseModel, minor fixes

--- models/courseModel.js
+++ models/courseModel.js
@@ -1,48 +1,18 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
-const term = {  
-  "2023-2024": "2023-2024",
-  "2024-2025": "2024-2025",
-  "2025-2026": "2025-2026",
-  "2026-2027": "2026-2027",
-};
-
 const courseSchema = new Schema({
   courseName: {
-    // Name of the course
     type: String,
     required: true,
     unique: true,
   },
   courseCode: {
-    // Subject code
     type: String,
     required: true,
     unique: true,
   },
-  professor: {
-    // Professor assigned to the course
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-  },
-  students: [
-    // Students enrolled in the course
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
-    },
-  ],
-  // Class schedule
-  days: {
-    // Days when the class occurs, e.g. [Monday, Wednesday, Friday]
-    type: [String],
-  },
-  term: {
-    type: String,
-    enum: Object.values(term),
-  },
 });
 
-const Course = models.users || model("courses", courseSchema);
+const Course = models.courses || model("courses", courseSchema);
 
 export default Course;
