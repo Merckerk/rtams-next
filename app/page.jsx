199478af2ba1a3dbe22fc7940045d80b618f2e0e
Merckerk <marckussantiago924@gmail.com>
2023-11-24 21:40:24 +0800

    chore: edited home page

--- app/page.jsx
+++ app/page.jsx
@@ -26,11 +26,6 @@ const Home = () => {
       description: "View, add and edit Courses.",
       routeName: "courses",
     },
-    {
-      featureName: "Teaching Load",
-      description: "Create, View, Edit, and Delete a teaching load on a professor.",
-      routeName: "teaching-load",
-    },
   ];
 
   return (
