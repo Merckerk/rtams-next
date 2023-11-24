14fdc1972e0e4455cb8ad050a8fea768f958d0a8
Merckerk <marckussantiago924@gmail.com>
2023-11-24 21:26:58 +0800

    feat: add teaching load done

--- app/teaching-load/page.jsx
+++ app/teaching-load/page.jsx
@@ -13,6 +13,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import ReusableInput from "@components/reusableInput/ReusableInput";
 
 import Link from "next/link";
 import axios from "axios";
@@ -45,6 +46,7 @@ const TeachingLoad = () => {
   const [loading, setLoading] = useState(false);
 
   const [coursesAPI, setCoursesAPI] = useState([]); //all courses
+  const [searchTerm, setSearchTerm] = useState("");
   const [editedLoad, setEditedLoad] = useState([]); //courses to be edited (ids of courses)
   const [userDetailsAPI, setUserDetailsAPI] = useState({
     image: "",
@@ -53,48 +55,102 @@ const TeachingLoad = () => {
     load: [],
   });
 
-  const fetchCoursesData = async () => {
-    const response = await axios.get("/api/courses/fetchCourses");
-    if (response) {
-      setCoursesAPI(response.data);
-    } else {
+  const filteredCourses = coursesAPI.filter(
+    (course) =>
+      course.courseCode.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      course.courseName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [coursesResponse, userDetailsResponse] = await Promise.all([
+          axios.get("/api/courses/fetchCourses"),
+          fetch(`/api/users/${userId}`),
+        ]);
+
+        const coursesData = coursesResponse.data;
+        const userData = await userDetailsResponse.json();
+
+        setCoursesAPI(coursesData);
+        setUserDetailsAPI({
+          image: userData.image,
+          name: userData.name,
+          email: userData.email,
+          load: userData.load,
+        });
+
+        setEditedLoad(userData?.load);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [userId]);
+
+  const handleEditLoad = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.patch(`/api/users/${userId}/editLoad`, {
+        load: editedLoad,
+      });
+
+      console.log("Load changes saved:", response.data);
+
+      router.push("/admin-users");
+    } catch (error) {
+      console.error("Error saving load changes:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const getUserDetails = async () => {
-    const response = await fetch(`/api/users/${userId}`);
-    const data = await response.json();
-    console.log(data);
-
-    setUserDetailsAPI({
-      image: data.image,
-      name: data.name,
-      email: data.email,
-      load: data.load,
-    });
-  };
-
   useEffect(() => {
-    fetchCoursesData();
-    getUserDetails();
-  }, []);
+    console.log("current load:", userDetailsAPI.load);
+    console.log("edited load:", editedLoad);
+  }, [userDetailsAPI.load, editedLoad]);
 
   const isCourseInLoad = (courseId) => userDetailsAPI.load.includes(courseId);
+
+  const isCourseInEditedLoad = (courseId) => editedLoad.includes(courseId);
+
+  const handleCheckboxChange = (courseId) => {
+    // Check if the course is already in the editedLoad array
+    const isCourseInLoad = editedLoad.includes(courseId);
+
+    // If the course is in the array, remove it; otherwise, add it
+    if (isCourseInLoad) {
+      setEditedLoad((prevLoad) => prevLoad.filter((id) => id !== courseId));
+    } else {
+      setEditedLoad((prevLoad) => [...prevLoad, courseId]);
+    }
+  };
 
   return (
     <div>
       <div className="flex flex-row items-center mb-10">
-        {userDetailsAPI.image && (
-          <div className="mr-4">
-            <Image
-              src={userDetailsAPI.image}
-              alt="User Image"
-              width={100}
-              height={100}
-              objectFit="contain"
-              className="rounded-full"
-            />
-          </div>
+        {!userDetailsAPI.image ? (
+          <Image
+            src={"/assets/images/defaultProfilePicture.jpg"}
+            alt="Default Profile"
+            width={100}
+            height={100}
+            objectFit="contain"
+            className="rounded-full"
+          />
+        ) : (
+          <Image
+            src={userDetailsAPI.image}
+            alt="User Profile"
+            width={100}
+            height={100}
+            objectFit="contain"
+            className="rounded-full"
+          />
         )}
         <div>
           <h1 className="text-3xl font-satoshi font-semibold text-gray-900">
@@ -105,6 +161,28 @@ const TeachingLoad = () => {
           </span>
         </div>
       </div>
+
+      <div className="flex-between">
+        <ReusableInput
+          label="Search"
+          type="text"
+          id="search"
+          name="search"
+          placeholder="Search by course code or name"
+          className="p-2 border border-gray-300 rounded w-64 m-3"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+        />
+
+        <button
+          className="black_btn"
+          onClick={handleEditLoad}
+          disabled={loading}
+        >
+          Save Load Changes
+        </button>
+      </div>
+
       <TableContainer component={Paper}>
         <Table
           className="min-w-[700px] md:min-w-screen-lg"
@@ -114,12 +192,16 @@ const TeachingLoad = () => {
             <TableRow>
               <StyledTableCell>Course Code</StyledTableCell>
               <StyledTableCell align="left">Course Name</StyledTableCell>
-              <StyledTableCell align="center">In Load</StyledTableCell>
-              <StyledTableCell align="center">Add or Remove Load</StyledTableCell>
+              <StyledTableCell align="center">
+                Current Load Status
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                Add or Remove Load
+              </StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {coursesAPI.map((course) => (
+            {filteredCourses.map((course) => (
               <StyledTableRow key={course._id}>
                 <StyledTableCell component="th" scope="row">
                   {course.courseCode}
@@ -143,7 +225,12 @@ const TeachingLoad = () => {
                 <StyledTableCell align="center">
                   {/* Use a checkbox for selecting courses */}
                   <FormControlLabel
-                    control={<Checkbox checked={isCourseInLoad(course._id)} onChange={() => {}} />}
+                    control={
+                      <Checkbox
+                        checked={isCourseInEditedLoad(course._id)}
+                        onChange={() => handleCheckboxChange(course._id)}
+                      />
+                    }
                   />
                 </StyledTableCell>
               </StyledTableRow>
