006402b38d4bd657e465015d170a95a1fc2889fa
Clint Nathan Calumpad <72256679+kwasonlover@users.noreply.github.com>
2023-11-28 11:30:14 +0800

    feat: route for searching a specific attendance report

---
+++ app/api/attendance/searchReport/route.js
@@ -0,0 +1,20 @@
+import AttendanceReport from "@models/attendanceModel";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const report = await AttendanceReport.findById({
+      name: params.id,
+      dateTime: params.id,
+    }).populate("name", "dateTime");
+
+    if (!report)
+      return new Response("Attendance Report not found.", { status: 404 });
+  } catch (error) {
+    return new Response("Failed to find the Attendance Report.", {
+      status: 500,
+    });
+  }
+};
