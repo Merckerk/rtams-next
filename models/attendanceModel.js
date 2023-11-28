394f5d9eb565f7b77d970561259c8874e8c49353
Clint Nathan Calumpad <72256679+kwasonlover@users.noreply.github.com>
2023-11-28 11:20:25 +0800

    chore: converted database schema fields to arrays

--- models/attendanceModel.js
+++ models/attendanceModel.js
@@ -1,34 +1,48 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
 const attendanceSchema = new Schema({
-  studentNumber: {
-    type: mongoose.Schema.ObjectId,
-    ref: "students",
-  },
-  name: {
-    type: mongoose.Schema.ObjectId,
-    ref: "students",
-  },
-  section: {
-    type: mongoose.Schema.ObjectId,
-    ref: "students",
-  },
-  nfcUID: {
-    type: mongoose.Schema.ObjectId,
-    ref: "students",
-  },
-  dateTime: {
-    type: Date,
-    required: true,
-  },
-  courseCode: {
-    type: mongoose.Schema.ObjectId,
-    ref: "courses",
-  },
-  term: {
-    type: mongoose.Schema.ObjectId,
-    ref: "courses",
-  },
+  studentNumber: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "students",
+    },
+  ],
+  name: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "students",
+    },
+  ],
+  section: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "students",
+    },
+  ],
+  nfcUID: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "students",
+    },
+  ],
+  dateTime: [
+    {
+      type: Date,
+      required: true,
+    },
+  ],
+  courseCode: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "courses",
+    },
+  ],
+  term: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "courses",
+    },
+  ],
 });
 
 const AttendanceReport =
