6f50a4846ab0e040f92128ade3567470242b2d12
Clint Nathan Calumpad <72256679+kwasonlover@users.noreply.github.com>
2023-12-08 00:37:48 +0800

    chore: middleware code added (will provide a fix later)

--- middleware.js
+++ middleware.js
@@ -1,3 +1,42 @@
-export { default } from 'next-auth/middleware';
+import { NextResponse } from "next/server";
 
-export const config = {matcher: ["/"]};
+export function middleware(request) {
+  const path = request.nextUrl.pathname;
+
+  const publicPath = path === "/login";
+
+  const token = request.cookies.get("next-auth.csrf-token")?.value || "";
+
+  if (publicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!publicPath && !token) {
+    console.log(
+      "This page is protected. Please log in to a valid account to continue."
+    );
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/login",
+    "/create-admin",
+    "/admin-users",
+    "/students",
+    "/courses",
+    "/attendances",
+    "/create-report",
+    "/create-student",
+    "/student-load",
+    "/teaching-load",
+    "/update-admin",
+    "/update-course",
+    "/update-student",
+    "/update-admin/:path",
+    "/update-course/:path",
+    "/update-student/:path",
+    "/",
+  ],
+};
