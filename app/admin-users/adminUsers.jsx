e1469638c31cc41dbded678a157c533419019fd3
Merckerk <marckussantiago924@gmail.com>
2024-01-24 18:43:05 +0800

    chore: forced to be dynamic

--- app/admin-users/adminUsers.jsx
+++ app/admin-users/adminUsers.jsx
@@ -16,6 +16,7 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0;
 export const fetchCache = 'force-no-store'
 
 const AdminUsers = () => {
