5e256b9f15014d75acc752b4e332ca321e54bfdb
Merckerk <marckussantiago924@gmail.com>
2024-01-24 18:13:36 +0800

    chore: changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    generateEtags: false,
+    onDemandEntries: {
+      maxInactiveAge: 5 * 1000, // 25 seconds
+      pagesBufferLength: 2,
+    },
+  };
