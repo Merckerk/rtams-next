e1384409d4d48cef2dc00fa3f0e061dac8641a29
Merckerk <marckussantiago924@gmail.com>
2024-01-27 23:58:52 +0800

    chore: protect admin

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    // "/create-admin",
+    "/create-admin",
     "/admin-users",
     "/students",
     "/courses",
