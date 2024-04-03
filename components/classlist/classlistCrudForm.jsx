8a9805ccc42cc7f03f7bbb24d37bc6827cbb6af4
Merckerk <marckussantiago924@gmail.com>
2024-04-03 13:06:13 +0800

    chore: suspense in classlist crud form

--- components/classlist/classlistCrudForm.jsx
+++ components/classlist/classlistCrudForm.jsx
@@ -13,7 +13,7 @@ import Checkbox from "@mui/material/Checkbox";
 
 import ReusableInput from "@components/reusableInput/ReusableInput";
 import ReusableDropdown from "@components/reusableDropdown/ReusableDropdown";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, Suspense } from "react";
 
 import Section from "@enums/section";
 
@@ -155,161 +155,163 @@ const ClasslistCrudForm = ({
   const onDelete = () => {
     areAllFieldsEmpty();
     handleDelete();
-  }
+  };
 
   return (
-    <div className="container mx-auto mt-5 mb-8">
-      <form className="max-w-2xl mb-8 mx-auto flex flex-col gap-7 glassmorphism">
-        <h1 className="text-3xl font-satoshi font-semibold text-gray-900">
-          {type} Class list
-        </h1>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container mx-auto mt-5 mb-8">
+        <form className="max-w-2xl mb-8 mx-auto flex flex-col gap-7 glassmorphism">
+          <h1 className="text-3xl font-satoshi font-semibold text-gray-900">
+            {type} Class list
+          </h1>
 
-        <ReusableDropdown
-          label="Faculty"
-          id="faculty"
-          name="faculty"
-          options={facultyOptions}
-          value={post?.user}
-          onChange={(e) => {
-            setPost({ ...post, user: e.target.value });
-          }}
-          placeholder="Select Faculty"
-        />
+          <ReusableDropdown
+            label="Faculty"
+            id="faculty"
+            name="faculty"
+            options={facultyOptions}
+            value={post?.user}
+            onChange={(e) => {
+              setPost({ ...post, user: e.target.value });
+            }}
+            placeholder="Select Faculty"
+          />
 
-        <ReusableDropdown
-          label="Section Code"
-          id="sectionCode"
-          name="sectionCode"
-          options={sectionOptions}
-          value={post?.sectionCode}
-          onChange={(e) => {
-            setPost({ ...post, sectionCode: e.target.value });
-          }}
-          placeholder="Select Section"
-        />
+          <ReusableDropdown
+            label="Section Code"
+            id="sectionCode"
+            name="sectionCode"
+            options={sectionOptions}
+            value={post?.sectionCode}
+            onChange={(e) => {
+              setPost({ ...post, sectionCode: e.target.value });
+            }}
+            placeholder="Select Section"
+          />
 
-        <ReusableInput
-          label="Subject Code"
-          type="text"
-          id="subjectCode"
-          name="subjectCode"
-          placeholder="Enter Subject Code"
-          className="form_input"
-          onChange={(e) => {
-            setPost({ ...post, subjectCode: e.target.value });
-            checkForEmptyValue(e.target.value, "subjectCode");
-          }}
-          value={post?.subjectCode}
-          errorMessage={errMsg.subjectCode}
-          required
-        />
+          <ReusableInput
+            label="Subject Code"
+            type="text"
+            id="subjectCode"
+            name="subjectCode"
+            placeholder="Enter Subject Code"
+            className="form_input"
+            onChange={(e) => {
+              setPost({ ...post, subjectCode: e.target.value });
+              checkForEmptyValue(e.target.value, "subjectCode");
+            }}
+            value={post?.subjectCode}
+            errorMessage={errMsg.subjectCode}
+            required
+          />
 
-        <ReusableInput
-          label="Subject Description"
-          type="text"
-          id="subjectDescription"
-          name="subjectDescription"
-          placeholder="Enter Subject Description"
-          className="form_input"
-          onChange={(e) => {
-            setPost({ ...post, subjectDescription: e.target.value });
-            checkForEmptyValue(e.target.value, "subjectDescription");
-          }}
-          value={post?.subjectDescription}
-          errorMessage={errMsg.subjectDescription}
-          required
-        />
+          <ReusableInput
+            label="Subject Description"
+            type="text"
+            id="subjectDescription"
+            name="subjectDescription"
+            placeholder="Enter Subject Description"
+            className="form_input"
+            onChange={(e) => {
+              setPost({ ...post, subjectDescription: e.target.value });
+              checkForEmptyValue(e.target.value, "subjectDescription");
+            }}
+            value={post?.subjectDescription}
+            errorMessage={errMsg.subjectDescription}
+            required
+          />
 
-        <ReusableInput
-          label="Term"
-          type="text"
-          id="term"
-          name="term"
-          placeholder="Enter Term"
-          className="form_input"
-          onChange={(e) => {
-            setPost({ ...post, term: e.target.value });
-          }}
-          value={post?.term}
-          errorMessage={errMsg.term}
-        />
+          <ReusableInput
+            label="Term"
+            type="text"
+            id="term"
+            name="term"
+            placeholder="Enter Term"
+            className="form_input"
+            onChange={(e) => {
+              setPost({ ...post, term: e.target.value });
+            }}
+            value={post?.term}
+            errorMessage={errMsg.term}
+          />
 
-        <ReusableInput
-          label="Schedule"
-          type="text"
-          id="schedule"
-          name="schedule"
-          placeholder="Schedule"
-          className="form_input"
-          onChange={(e) => {
-            setPost({ ...post, schedule: e.target.value });
-          }}
-          value={post?.schedule}
-          errorMessage={errMsg.schedule}
-        />
+          <ReusableInput
+            label="Schedule"
+            type="text"
+            id="schedule"
+            name="schedule"
+            placeholder="Schedule"
+            className="form_input"
+            onChange={(e) => {
+              setPost({ ...post, schedule: e.target.value });
+            }}
+            value={post?.schedule}
+            errorMessage={errMsg.schedule}
+          />
 
-        {type == "Edit" ? (
-          <label>
-            <span className="font-satoshi font-semibold text-base text-gray-700">
-              Reason to Edit or Delete:
-            </span>
-            <textarea
-              id="audit"
-              name="audit"
-              placeholder="Your reason for editing/deleting here..."
-              className="form_input"
-              onChange={(e) => {
-                setPost({ ...post, audit: e.target.value });
-                validateAudit(e.target.value, "audit");
-              }}
-              value={post?.audit}
-            />
-            {errMsg.audit ? (
-              <p className="error_message">{errMsg.audit}</p>
-            ) : null}
-          </label>
-        ) : null}
-      </form>
-
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl font-satoshi font-semibold text-gray-900 pb-7">
-          Class list
-        </h1>
-
-        <div className="flex">
-          <button className="pb-7 mx-2 black_btn" onClick={handleEdit}>
-            {loading ? "Processing" : `${type} Class List`}
-          </button>
           {type == "Edit" ? (
-            <button
-              className="red_btn ml-2"
-              disabled={loading}
-              onClick={onDelete}
-            >
-              {loading ? "Processing" : "Delete Classlist"}
-            </button>
+            <label>
+              <span className="font-satoshi font-semibold text-base text-gray-700">
+                Reason to Edit or Delete:
+              </span>
+              <textarea
+                id="audit"
+                name="audit"
+                placeholder="Your reason for editing/deleting here..."
+                className="form_input"
+                onChange={(e) => {
+                  setPost({ ...post, audit: e.target.value });
+                  validateAudit(e.target.value, "audit");
+                }}
+                value={post?.audit}
+              />
+              {errMsg.audit ? (
+                <p className="error_message">{errMsg.audit}</p>
+              ) : null}
+            </label>
           ) : null}
-        </div>
-      </div>
+        </form>
 
-      <TableContainer component={Paper}>
-        <Table
-          className="min-w-[700px] md:min-w-screen-lg"
-          aria-label="admin users table"
-        >
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Student Number</StyledTableCell>
-              <StyledTableCell align="left">Name</StyledTableCell>
-              <StyledTableCell align="left">Section</StyledTableCell>
-              <StyledTableCell align="left">Status</StyledTableCell>
-              <StyledTableCell align="center">Actions</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>{memoizedStudents}</TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-satoshi font-semibold text-gray-900 pb-7">
+            Class list
+          </h1>
+
+          <div className="flex">
+            <button className="pb-7 mx-2 black_btn" onClick={handleEdit}>
+              {loading ? "Processing" : `${type} Class List`}
+            </button>
+            {type == "Edit" ? (
+              <button
+                className="red_btn ml-2"
+                disabled={loading}
+                onClick={onDelete}
+              >
+                {loading ? "Processing" : "Delete Classlist"}
+              </button>
+            ) : null}
+          </div>
+        </div>
+
+        <TableContainer component={Paper}>
+          <Table
+            className="min-w-[700px] md:min-w-screen-lg"
+            aria-label="admin users table"
+          >
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Student Number</StyledTableCell>
+                <StyledTableCell align="left">Name</StyledTableCell>
+                <StyledTableCell align="left">Section</StyledTableCell>
+                <StyledTableCell align="left">Status</StyledTableCell>
+                <StyledTableCell align="center">Actions</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{memoizedStudents}</TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </Suspense>
   );
 };
 
