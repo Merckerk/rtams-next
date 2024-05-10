39920475d338de082ec3a999a8435d3e44f8ff12
Merckerk <marckussantiago924@gmail.com>
2024-05-10 17:01:59 +0800

    chore: set max duration

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -9,6 +9,8 @@ export const revalidate = 0;
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
+export const maxDuration = 60;
+
 export const GET = async (req, res) => {
   try {
     await connectToDB();
