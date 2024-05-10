1cf556b6e41d72e8a6a9d2912c2970a64714570c
Merckerk <marckussantiago924@gmail.com>
2024-05-10 14:15:50 +0800

    feat: POC nodemailer

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -2,6 +2,7 @@ import { connectToDB } from "@utils/database";
 import Attendances from "@models/attendanceModel";
 import Session from "@models/sessionModel";
 import User from "@models/userModel";
+import { transporter } from "@utils/nodemailer";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
@@ -17,6 +18,17 @@ export const GET = async (req, res) => {
         : null;
     const filteredUsersEmail = [...new Set(usersEmail)];
     console.log(filteredUsersEmail);
+
+    const mailOptions = {
+      from: process.env.NM_EMAIL,
+      to: filteredUsersEmail,
+    };
+
+    await transporter.sendMail({
+      ...mailOptions,
+      subject: "nodemailer test",
+      text: "Student attendance have been evaluated recently and the following classlists have students to not meet the required attendance threshold: [list ng classes to be implemented later]",
+    });
 
     return new Response(JSON.stringify(filteredUsersEmail), { status: 500 });
   } catch (error) {
